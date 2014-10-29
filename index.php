48ff331548f5b703de443a69b0f50cca1b838c0f
Vivian <Vivian@Vivian-2.local>
2014-10-29 00:56:03 -0700

    set up index.php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-index.php
+<?php get_header(); ?>
+
+<div class="content">
+
+<div class="article">
+
+</div>
+
+<div class="sidebar">
+
+<?php get_sidebar(); ?>
+
+</div>
+
+</div>
+
+<?php get_footer(); ?>
